a8dcf6f3244f42c43de7b084b8374bcea63d1131
Harshvardha Rathore <rharshvardhan614@gmail.com>
2025-05-15 07:27:56 +0530

    added the quest images and create the quest card component

---
+++ src/app/components/common/questCard/index.tsx
@@ -0,0 +1,52 @@
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import './questCard.scss';
+import Link from 'next/link';
+
+interface QuestCardProps {
+  href: string;
+  backgroundImage: string;
+  coinIcon: string;
+  rewardAmount: number;
+  title: string;
+  tag: string;
+  chadsCount: string;
+  boardLabel: string;
+}
+
+const QuestCard: React.FC<QuestCardProps> = ({
+  href,
+  backgroundImage: backgroundImage,
+  coinIcon,
+  rewardAmount,
+  title,
+  tag,
+  chadsCount,
+  boardLabel,
+}) => (
+  <Link href={href} className="quest-link">
+    <div className="quest-card">
+      <div className="image-wrapper">
+        <Image src={backgroundImage} alt="Quest Background" fill className={'image-background'} />
+        <div className="reward-tag">
+          <div className="reward-badge">
+            <Image src={coinIcon} alt="coin" width={25} height={20} className="coin-icon" />
+            <span className="reward-text">{rewardAmount}</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="card-content">
+        <h3 className="title">{title}</h3>
+        <span className="quest-tag">{tag}</span>
+        <div className="meta">
+          <span className="chads">{chadsCount}</span>
+          <span className="board">{boardLabel}</span>
+        </div>
+      </div>
+    </div>
+  </Link>
+);
+
+export default QuestCard;
